bfc8890142bce448451dc04aa06aaf0c9466b4c7
Angelene Atienza <marielle_angelene_atienza@dlsu.edu.ph>
2024-07-17 22:39:53 +0800

    redirect to Login Page on first access, added bg to assets

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
+      redirect: '/login'
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue') // Lazy-load the login view
+      component: LoginView // updated to use direct import
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView
     },
     {
       path: '/threads',
