3ee997994b63830ca1a031b19bb6017757eedd89
fatima_dy <136289175+pateemaa@users.noreply.github.com>
2024-07-24 15:48:31 +0800

    made skeletal code for automation testing

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,8 +1,78 @@
 // https://on.cypress.io/api
 
-describe('My First Test', () => {
-  it('visits the app root url', () => {
+describe('Automation Test for ClassRank', () => {
+  beforeEach(()=> {
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.contains()
+  })
+
+  describe('Account Registration Test', () => {
+    
+    it('Admin Account Registration Successful', () => {
+      cy.get('[]').type('{enter}')
+      cy.contains()
+    })  
+
+    it('Admin Account Registration Failed', () => {
+      cy.get('[]').type('{enter}')
+      cy.contains()
+    })
+
+    it('Student Account Registration Successful', () => {
+      cy.get('[]').type('{enter}')
+      cy.contains()
+    })
+
+    it('Student Account Registration Failed', () => {
+      cy.get('[]').type('{enter}')
+      cy.contains()
+    })
+  })
+
+  describe('Login/Logout Test', () => {
+    beforeEach(() => {
+      cy.get('[]').type('')
+      cy.get('[]').type('')
+    })
+
+    it('Login Successful', () => {
+      cy.get().should()
+    })
+
+    it('Login Failed', () => {
+      cy.get().should()
+    })
+
+    it('Logout Successful', () => {
+      cy.get().should()
+    })
+  })
+
+  describe('Main Features Test', () => {
+    it('Create New Thread', () => {
+      cy.get().should()
+    })
+
+    it('Edit Thread', () => {
+      cy.get().should()
+    })
+
+    it('Delete Thread', () => {
+      cy.get().should()
+    })
+  })
+
+  describe('Sub Features Test', () => {
+    it('Add Comment', () => {
+      cy.get().should()
+    })
+
+    it('Edit Comment', () => {
+      cy.get().should()
+    })
+
+    it('Delete Comment', () => {
+      cy.get().should()
+    })
   })
 })
